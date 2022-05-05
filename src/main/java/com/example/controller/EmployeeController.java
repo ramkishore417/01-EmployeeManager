78bcae1bb2540026fcd8ca081bdeeebba647ae1b
RAMKISHORE DYAVANPALLY <48268023+ramkishore417@users.noreply.github.com>
2022-05-05 19:13:03 +0530

    Update EmployeeController.java

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employee")
 public class EmployeeController
 {
-    private final EmployeeService employeeService;
+    private final EmployeeService employeeService1;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
