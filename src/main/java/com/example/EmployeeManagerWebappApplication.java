ef54d4234ca96a06205d13c65e9132209e045533
RAMKISHORE DYAVANPALLY <48268023+Ramkishore417@users.noreply.github.com>
2022-05-06 11:15:24 +0530

    Initial commit

--- src/main/java/com/example/EmployeeManagerWebappApplication.java
+++ src/main/java/com/example/EmployeeManagerWebappApplication.java
@@ -14,7 +14,7 @@ public class EmployeeManagerWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerWebappApplication.class, args);
-
+		System.out.println("Hello");
 	}
 
 	@Bean
